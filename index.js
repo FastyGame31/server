8c4bae0bc0d7138bfc62358f906d0334f635b93c
FastyGame31 <81424889+FastyGame31@users.noreply.github.com>
2024-12-27 13:13:32 +0400

    Update index.js

--- index.js
+++ index.js
@@ -47,6 +47,7 @@ app.post('/key', (req, res) => {
 
 // Start the server
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
+
